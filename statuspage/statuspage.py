5a20b83f4f2cac7274af8a643e12f5371e98206a
Mike Wendt <mike.wendt@me.com>
2019-11-07 12:32:40 -0500

    FIX Match hex formatting

--- statuspage/statuspage.py
+++ statuspage/statuspage.py
@@ -28,7 +28,7 @@ COLORED_LABELS = (
     ("1192FC", "investigating",),
     ("FFA500", "degraded performance"),
     ("FF4D4D", "major outage",),
-    ("44c8e5", "planned maintenance"),
+    ("44C8E5", "planned maintenance"),
     ("8914CC", "unplanned maintenance")
 )
 
